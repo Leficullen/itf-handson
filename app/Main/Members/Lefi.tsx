a95576ecd8f6d4d102ae0228f0930aefd4dd1ccd
Leficullen <lefirizqy2007@gmail.com>
2026-06-22 14:32:46 +0700

    init

---
+++ app/Main/Members/Lefi.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Lefi() {
+     return (
+          <div className="hover:border-black border-zinc-500 border-3 bg-zinc-400 space-y-4 px-3 py-10 justify-center mx-auto rounded-xl w-full h-full transition-color duration-100 ease-in-out">
+               <h1 className=" text-white font-bold px-5 py-2 text-xl">
+                    Muh. Alfi Rizqy
+               </h1>
+               <button className="text-white hover:bg-white hover:text-black  font-semibold border cursor-pointer bg-black rounded-xl text-sm px-3 py-2 mx-auto justify-center w-full" onClick={()=> window.open("https://www.instagram.com/leficullen.tsx/", "_blank", "noopener,noreferrer")} >
+                    View my ig
+               </button>
+          </div>
+     )
+
+}
---
+++ app/Main/main.tsx
@@ -0,0 +1,14 @@
+import Lefi from "./Members/Lefi"
+
+export function Welcome() {
+  return (
+    <main className="flex justify-center pt-16 pb-4">
+      <div className="grid grid-cols-5 gap-4 py-10 mx-2">
+        <Lefi/>
+  
+      </div>
+    </main>
+  );
+}
+
+
