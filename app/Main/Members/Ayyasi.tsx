44190f1638f466702704c315fa68a4e8f75701de
Asepgunawan112 <ayyasi52@gmail.com>
2026-06-23 21:07:58 +0700

    Commit Ayyasi (Card pake template dulu, nanti dikreasikan)

---
+++ app/Main/Members/Ayyasi.tsx
@@ -0,0 +1,19 @@
+export default function Ayyasi() {
+  return (
+    <div className="hover:border-black border-zinc-500 border-3 bg-zinc-400 space-y-4 px-3 py-10 justify-center mx-auto rounded-xl w-full h-full transition-color duration-100 ease-in-out">
+      <h1 className=" text-white font-bold px-5 py-2 text-xl">Ayyasi</h1>
+      <button
+        className="text-white hover:bg-white hover:text-black  font-semibold border cursor-pointer bg-black rounded-xl text-sm px-3 py-2 mx-auto justify-center w-full"
+        onClick={() =>
+          window.open(
+            "https://www.instagram.com/ayyasii/",
+            "_blank",
+            "noopener,noreferrer",
+          )
+        }
+      >
+        View my ig
+      </button>
+    </div>
+  );
+}
--- app/Main/main.tsx
+++ app/Main/main.tsx
@@ -1,14 +1,13 @@
-import Lefi from "./Members/Lefi"
+import Lefi from "./Members/Lefi";
+import Ayyasi from "./Members/Ayyasi";
 
 export function Welcome() {
   return (
     <main className="flex justify-center pt-16 pb-4">
       <div className="grid grid-cols-5 gap-4 py-10 mx-2">
-        <Lefi/>
-  
+        <Lefi />
+        <Ayyasi />
       </div>
     </main>
   );
 }
-
-
