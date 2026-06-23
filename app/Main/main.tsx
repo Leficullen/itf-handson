f700d3a548f3ec71d2dcef6ce5c4bfe3ea096f6f
Muhamad Ayrazhan <muhamad.ayrazhan@ui.ac.id>
2026-06-23 20:51:02 +0700

    zhan

--- app/Main/main.tsx
+++ app/Main/main.tsx
@@ -1,11 +1,12 @@
 import Lefi from "./Members/Lefi"
+import Zhan from "./Members/Lefi"
 
 export function Welcome() {
   return (
     <main className="flex justify-center pt-16 pb-4">
       <div className="grid grid-cols-5 gap-4 py-10 mx-2">
         <Lefi/>
-  
+        <Zhan/>
       </div>
     </main>
   );
