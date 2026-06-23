14f3a51094d66b99dc40c1ae8e447fc773c13d2a
iD3sk <fiqhideski@gmail.com>
2026-06-23 20:53:55 +0700

    fiqhi

--- app/Main/main.tsx
+++ app/Main/main.tsx
@@ -1,11 +1,12 @@
 import Lefi from "./Members/Lefi"
+import Fiqhi from "./Members/Fiqhi"
 
 export function Welcome() {
   return (
     <main className="flex justify-center pt-16 pb-4">
       <div className="grid grid-cols-5 gap-4 py-10 mx-2">
         <Lefi/>
-  
+        <Fiqhi/>
       </div>
     </main>
   );
