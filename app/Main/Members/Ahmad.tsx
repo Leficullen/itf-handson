260bef75fc77f5191642d066154acc243a6c2f00
aszorya <ahmadszorya@gmail.com>
2026-06-23 20:28:38 +0700

    feat/ahmad

--- app/Main/Members/Ahmad.tsx
+++ app/Main/Members/Ahmad.tsx
@@ -1,12 +1,10 @@
-
-
-export default function Lefi() {
+export default function Ahmad() {
      return (
           <div className="hover:border-black border-zinc-500 border-3 bg-zinc-400 space-y-4 px-3 py-10 justify-center mx-auto rounded-xl w-full h-full transition-color duration-100 ease-in-out">
                <h1 className=" text-white font-bold px-5 py-2 text-xl">
-                    Muh. Alfi Rizqy
+	       	Ahmad Shanahan Zorya
                </h1>
-               <button className="text-white hover:bg-white hover:text-black  font-semibold border cursor-pointer bg-black rounded-xl text-sm px-3 py-2 mx-auto justify-center w-full" onClick={()=> window.open("https://www.instagram.com/leficullen.tsx/", "_blank", "noopener,noreferrer")} >
+               <button className="text-white hover:bg-white hover:text-black  font-semibold border cursor-pointer bg-black rounded-xl text-sm px-3 py-2 mx-auto justify-center w-full" onClick={()=> window.open("https://www.instagram.com/aszorya/", "_blank", "noopener,noreferrer")} >
                     View my ig
                </button>
           </div>
--- app/Main/main.tsx
+++ app/Main/main.tsx
@@ -1,10 +1,11 @@
-import Lefi from "./Members/Lefi"
+import Ahmad from "./Members/Ahmad"
+import Tipen from "./Members/Tipen"
 
 export function Welcome() {
   return (
     <main className="flex justify-center pt-16 pb-4">
       <div className="grid grid-cols-5 gap-4 py-10 mx-2">
-        <Lefi/>
+        <Ahmad/>
   
       </div>
     </main>
