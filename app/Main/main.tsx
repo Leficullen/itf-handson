4a6d49c85deb94eebef1c8c9f06ebca7583cc4a4
Syakirahzd <syaasiui@gmail.com>
2026-06-23 20:51:15 +0700

    handson itf - syak

--- app/Main/main.tsx
+++ app/Main/main.tsx
@@ -1,11 +1,12 @@
 import Lefi from "./Members/Lefi"
+import Syakirah from "./Members/Syakirah"
 
 export function Welcome() {
   return (
     <main className="flex justify-center pt-16 pb-4">
       <div className="grid grid-cols-5 gap-4 py-10 mx-2">
         <Lefi/>
-  
+        <Syakirah/>
       </div>
     </main>
   );
